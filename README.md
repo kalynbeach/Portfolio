4164d235de048195ce22191a3f04901377fe4543
Kalyn Beach <kalynbeach@gmail.com>
2019-02-04 23:50:54 -0800

    Add favicon, update README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Currently a work in progress!
 
+Built with GatsbyJS, React, GraphQL, and Sass.
+
 [kalynbeach.com](https://kalynbeach.com)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   siteMetadata: {
-    title: 'Kalyn Beach - Portfolio',
+    title: 'Kalyn Beach',
     description: '',
     author: '@kalynbeach',
   },
@@ -27,7 +27,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon-32x32.png', // This path is relative to the root of the site.
       },
     },
     {
