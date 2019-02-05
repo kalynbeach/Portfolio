3dcf683724e326f654e4c19aa829e1f9a6b94657
Kalyn Beach <kalynbeach@gmail.com>
2019-02-04 17:11:37 -0800

    Adjust font and layout styles

---
+++ src/html.jsx
@@ -0,0 +1,53 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              #___gatsby {
+                display: flex;
+                flex-direction: column;
+                min-height: 100%;
+              }
+              #___gatsby>div {
+                min-height: 100%;
+              }
+              `
+          }}
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <noscript key="noscript" id="gatsby-noscript">
+          This app works best with JavaScript enabled.
+        </noscript>
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array
+};
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,13 +2,13 @@ import Typography from 'typography';
 import sutro from 'typography-theme-sutro';
 
 sutro.overrideThemeStyles = ({
-  adjustFontSizeTo,
   rhythm
 }) => ({
   'h1,h2,h3,h4,h5,h6': {
-    ...adjustFontSizeTo(rhythm(1)),
     marginBottom: rhythm(1),
-    marginTop: rhythm(1)
+    marginTop: rhythm(1),
+    color: '#201f1f',
+    fontWeight: 900
   }
 });
 
