d9758a7c4b2e8a3c714372d11565ed4e62d6c464
Kalyn Beach <kalynbeach@gmail.com>
2019-02-09 15:46:03 -0800

    Add Google Analytics, load env vars in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,57 @@
 // eslint-disable-next-line no-undef
+require("dotenv").config({
+  // eslint-disable-next-line no-undef
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+// eslint-disable-next-line no-undef
 module.exports = {
   siteMetadata: {
-    title: 'Kalyn Beach',
-    description: '',
-    author: '@kalynbeach',
+    title: "Kalyn Beach",
+    description: "",
+    author: "@kalynbeach"
   },
   plugins: [
-    'gatsby-plugin-eslint',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-eslint",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        name: 'images',
         // eslint-disable-next-line no-undef
-        path: `${__dirname}/src/images`,
-      },
+        trackingId: `${process.env.PORTFOLIO_GA_ID}`
+      }
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'kalyn-beach-portfolio',
-        short_name: 'portfolio',
-        start_url: '/',
-        background_color: '#FFFFFF',
-        theme_color: '#F92300',
-        display: 'minimal-ui',
-        icon: 'src/images/favicon-32x32.png', // This path is relative to the root of the site.
-      },
+        name: "images",
+        // eslint-disable-next-line no-undef
+        path: `${__dirname}/src/images`
+      }
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "kalyn-beach-portfolio",
+        short_name: "portfolio",
+        start_url: "/",
+        background_color: "#FFFFFF",
+        theme_color: "#F92300",
+        display: "minimal-ui",
+        icon: "src/images/favicon-32x32.png" // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
-}
+  ]
+};
